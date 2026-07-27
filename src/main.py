fd8f376d42a86ff473da18d62db3dbdf5ddcf6f3
EpikViper <sandroturmanidze50@gmail.com>
2026-07-27 21:54:08 +0400

    Docs commit

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from textnode import TextNode, TextType
 from static_to_public import static_to_public
 from structure_page import generate_page, generate_page_recursive
+import sys
 
 def main():
-    static_to_public("/home/turmana/static-website-generator/static", "/home/turmana/static-website-generator/public")
+    base_path = sys.argv[0]
+    static_to_public("/home/turmana/static-website-generator/static", "/home/turmana/static-website-generator/docs")
     generate_page_recursive("/home/turmana/static-website-generator/content", "/home/turmana/static-website-generator/template.html",
-    "/home/turmana/static-website-generator/public")
+    "/home/turmana/static-website-generator/docs", base_path)
     
 
 main()
--- src/structure_page.py
+++ src/structure_page.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
     raise Exception()
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path="/"):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f:
         from_file = f.read()
@@ -22,6 +22,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(from_file)
     template_file = template_file.replace("{{ Title }}", title)
     template_file = template_file.replace("{{ Content }}", html)
+    template_file = template_file.replace('href="/', base_path)
+    template_file = template_file.replace('src="/', base_path)
 
     dest_dir = os.path.dirname(dest_path)
     if dest_dir:
@@ -30,7 +32,7 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as d:
         d.write(template_file)
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, base_path='/'):
     for unit in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, unit)
         to_path = os.path.join(dest_dir_path, unit)
@@ -38,4 +40,4 @@ def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
             if unit.endswith(".md"):
                 generate_page(from_path, template_path, to_path[:-3] + ".html")
         elif os.path.isdir(from_path):
-            generate_page_recursive(from_path, template_path, to_path)
+            generate_page_recursive(from_path, template_path, to_path, base_path)
